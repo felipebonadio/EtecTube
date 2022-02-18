a8386ace6224b31789626cbd985844a3ffcdd47d
unknown <febonadio@gmail.com>
2022-02-18 20:35:36 -0300

    criacao index.css e alteracao do local do script

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function ShowHideSideBar(){
+    let sideBar = document.getElementById("sideBar");
+    if(sideBar.style.visibility === "visible"){
+        sideBar.style.visibility = "hidden";
+        sideBar.style.width = 0;
+    }else{
+        sideBar.style.visibility = "visible";
+        sideBar.style.width = "17%";
+    }
+}
