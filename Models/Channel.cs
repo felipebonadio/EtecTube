b195383a78fa24b9fabb2a3a37eb6b20755f6345
unknown <febonadio@gmail.com>
2022-02-25 19:25:33 -0300

    atualizacao do projeto

---
+++ Models/Channel.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EtecTube.Models
+{
+    [Table("Channel")]
+    public class Channel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Nome")]
+        public string Name { get; set; }
+
+        [StringLength(200)]
+        [Display(Name = "Foto do Canal")]
+        public string ChannelPicture { get; set; }
+    }
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,13 +1,10 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-function ShowHideSideBar(){
+﻿
+function ShowHideSideBar() {
     let sideBar = document.getElementById("sideBar");
-    if(sideBar.style.visibility === "visible"){
+    if (sideBar.style.visibility === "visible") {
         sideBar.style.visibility = "hidden";
         sideBar.style.width = 0;
-    }else{
+    } else  {
         sideBar.style.visibility = "visible";
         sideBar.style.width = "17%";
     }
